57d65b77b5166d0bd84a1ca4defad2b7097961e0
Rikin Katyal <katyalrikin@gmail.com>
2017-04-28 12:03:26 -0400

    Login UI Almost Complete

--- app/src/main/java/com/sirvar/robin/demo/MainActivity.java
+++ app/src/main/java/com/sirvar/robin/demo/MainActivity.java
@@ -1,18 +1,20 @@
 package com.sirvar.robin.demo;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 
-import com.sirvar.robin.LoginActivity;
+import com.sirvar.robin.SigninActivity;
 import com.sirvar.robin.Theme;
 
-public class MainActivity extends LoginActivity {
+public class MainActivity extends SigninActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setTitle("Login");
+        setTitle("Sign in to Robin");
         setImage(getResources().getDrawable(R.mipmap.ic_launcher));
+        setFont(Typeface.createFromAsset(getAssets(), "Montserrat-Medium.ttf"));
         setTheme(Theme.LIGHT);
     }
 
--- robin/src/main/java/com/sirvar/robin/SigninActivity.java
+++ robin/src/main/java/com/sirvar/robin/SigninActivity.java
@@ -1,30 +1,54 @@
 package com.sirvar.robin;
 
 import android.graphics.Bitmap;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public abstract class LoginActivity extends AppCompatActivity {
+public abstract class SigninActivity extends AppCompatActivity {
 
     private TextView title;
+    private TextView signup;
     private ImageView logo;
     private EditText email;
     private EditText password;
+    private TextInputLayout emailWrapper;
+    private TextInputLayout passwordWrapper;
+    private Button submit;
     private Theme theme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_robin_login);
+        setContentView(R.layout.activity_robin_signin);
 
+        // Don't show toolbar
         getSupportActionBar().hide();
 
+        // Initialize views
         title = (TextView) findViewById(R.id.title);
+        signup = (TextView) findViewById(R.id.signup);
         logo = (ImageView) findViewById(R.id.logo);
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        emailWrapper = (TextInputLayout) findViewById(R.id.wrapper_email);
+        passwordWrapper = (TextInputLayout) findViewById(R.id.wrapper_password);
+        submit = (Button) findViewById(R.id.submit);
+
+        // Login form submitted
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onSubmit(email.getText().toString(), password.getText().toString());
+            }
+        });
 
         setDefaults();
     }
@@ -70,10 +94,24 @@ public abstract class LoginActivity extends AppCompatActivity {
     }
 
     /**
+     * Use custom font for all Views
+     * @param typeface custom typeface
+     */
+    protected void setFont(Typeface typeface) {
+        title.setTypeface(typeface);
+        signup.setTypeface(typeface);
+        email.setTypeface(typeface);
+        password.setTypeface(typeface);
+        emailWrapper.setTypeface(typeface);
+        passwordWrapper.setTypeface(typeface);
+        submit.setTypeface(typeface);
+    }
+
+    /**
      * Set default values
      */
     private void setDefaults() {
-        title.setText(R.string.login);
+        title.setText(R.string.signin);
         theme = Theme.LIGHT;
     }
 }
